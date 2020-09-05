ac557ae4ca1eb26dfe44b1f42cadeb670a38b78c
anu43 <56151687+anu43@users.noreply.github.com>
2020-09-05 23:26:09 +0300

    added learning mode

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 from collections import defaultdict
 from bs4 import BeautifulSoup
 from functools import reduce
+from numpy import random
+import pandas as pd
 import requests
 import codecs
 import json
+import sys
 
 
 def getLower(input: str) -> str:
@@ -49,6 +52,32 @@ def writeJSON(data: dict, filename: str):
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
+def calc_prob_dist(d: dict) -> list:
+    # Declare an empty list
+    t_showns = list()
+    # Declare words
+    words = d.keys()
+    # Iterate through words of dict
+    for word in words:
+        # Append how many times it is shown variable
+        t_showns.append(d[word]['times_shown'])
+    # Create a frame of words and the number of exhibitions
+    df = pd.DataFrame(data=zip(words, t_showns), columns=['words', 'times_shown'])
+    # Create a new frame which is sorted by times_shown column
+    df2 = df.sort_values(by='times_shown', ascending=True)
+    # Add an additional column of values which are ascending=False to df2
+    df2['desc_times_shown'] = df2.sort_values(
+        by='times_shown', ascending=False
+    ).times_shown.to_numpy()
+    # Calculate the total number in times_shown column
+    total = df.times_shown.sum()
+    # Calculate the probabilities
+    df2['probs'] = df2.desc_times_shown.apply(lambda x: x / total)
+
+    # Return words list and probability distribution
+    return df2.words.to_numpy(), df2.probs.to_numpy()
+
+
 # Declare URLs to request
 URL_LEX = 'https://www.lexico.com/definition/'
 URL_TUR = 'https://tureng.com/en/turkish-english/'
@@ -58,29 +87,58 @@ d = readJSON('dict.json')  # JSON file
 
 # Iterate through words
 for word in readTextFile('inp.txt'):
-    # If word not in dict.json
-    if word not in d:
-        d[word] = defaultdict(list, {'English': list(), 'Turkish': list()})
-        # Read Lexico website
-        lexico = requests.get(URL_LEX + word).text
-        # Create BeautifulSoup obj of the Lexico website
-        soup = BeautifulSoup(lexico, 'lxml')
-        # Find the section of the page
-        section = soup.find('section')
-        # Iterate through class 'ind'
-        for idx, cls_ind in enumerate(section.find_all('span', class_='ind')):
-            # Append English meanings to dictionary
-            d[word]['English'].append(str(cls_ind.text))
-        # Read TurEng website
-        tureng = requests.get(URL_TUR + word).text
-        # Create BeautifulSoup obj of the TurEng website
-        soup = BeautifulSoup(tureng, 'lxml')
-        # Find the table of the page
-        table = soup.find('table')
-        # Iterate through class 'tr ts'
-        for idx, cls_ind in enumerate(table.find_all('td', class_='tr ts')):
-            # Append English meanings to dictionary
-            d[word]['Turkish'].append(str(cls_ind.text))
+    # If -update arg is received
+    if sys.argv[1] == '-update':
+        # If word not in dict.json
+        if word not in d:
+            d[word] = defaultdict(list, {
+                'English': list(), 'Turkish': list(), 'times_shown': 0
+            })
+            # Read Lexico website
+            lexico = requests.get(URL_LEX + word).text
+            # Create BeautifulSoup obj of the Lexico website
+            soup = BeautifulSoup(lexico, 'lxml')
+            # Find the section of the page
+            section = soup.find('section')
+            # Iterate through class 'ind'
+            for idx, cls_ind in enumerate(section.find_all('span', class_='ind')):
+                # Append English meanings to dictionary
+                d[word]['English'].append(str(cls_ind.text))
+            # Read TurEng website
+            tureng = requests.get(URL_TUR + word).text
+            # Create BeautifulSoup obj of the TurEng website
+            soup = BeautifulSoup(tureng, 'lxml')
+            # Find the table of the page
+            table = soup.find('table')
+            # Iterate through class 'tr ts'
+            for idx, cls_ind in enumerate(table.find_all('td', class_='tr ts')):
+                # Append English meanings to dictionary
+                d[word]['Turkish'].append(str(cls_ind.text))
+    # If -learn arg is received
+    elif sys.argv[1] == '-learn':
+        # Declare the prob distribution
+        words, prob_dist = calc_prob_dist(d)
+        # Get a sample according to the given arg
+        for idx, word in enumerate(random.choice(words, size=int(sys.argv[2]),
+                                                 replace=False, p=prob_dist)):
+            # Wait for enter to show the meanings
+            inp = input(f'{word.upper()}')
+            # If it is enter
+            if inp == '':
+                # Display English meanings
+                # Print in a format
+                print('English:')
+                for eng_meaning in d[word]['English']:
+                    # Print English meanings
+                    print(f'\t{idx}. {eng_meaning}')
+                # Display Turkish meanings
+                # Print in a format
+                print('Turkish:')
+                for tur_meaning in d[word]['Turkish']:
+                    # Print Turkish meanings
+                    print(f'\t{idx}. {tur_meaning}')
+        # End the session after the number of given arg
+        break
 
-# Write JSON file
+        # Write JSON file
 writeJSON(d, 'dict')
